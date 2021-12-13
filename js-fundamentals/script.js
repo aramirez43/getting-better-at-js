949cebfd51facc0e4878f2c7c0e484c4272aeb6f
Alan Ramirez <aramireznicola@cnm.edu>
2021-12-13 16:57:17 -0700

    just finished let,const,and var

---
+++ js-fundamentals/script.js
@@ -0,0 +1,66 @@
+//Assignment 1
+// let country = 'United States'
+// let continent = 'North America'
+// let population = '329.5 million'
+//
+// console.log(country)
+// console.log(population)
+// console.log(continent)
+
+//Data Types
+//Number = floating point numbers = used for decimal and integers
+//String = sequence of characters = used for text
+//Boolean = logical type that can only be true or false = used for taking decisions
+//undefined = value taken by a variable that is not yet defined "empty value"
+//Null = 'empty value'
+//Symbol = value that is unique and cannot be changed
+//BigInt = larger integers than the number type can hold
+
+// Boolean
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+//
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Alan');
+
+//Dynamic Typing
+// javascriptIsFun = 'YES!';
+// console.log(javascriptIsFun);
+
+//Undefined
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+//Assignment 2
+// let isIsland = false;
+// let language;
+// console.log(isIsland)
+// console.log(population)
+// console.log(country)
+// console.log(language)
+
+//let,const,and var
+//let can be changed later
+//const cannot be changed
+//var should be avoided but is used for legacy
+//let is block scoped and var is function scoped
+
+// let age = 30;
+// age = 31;
+//
+// const birthYear = 1991;
+// birthYear = 1990
+//
+// var job = 'programmer';
+// job = 'teacher';
+
+//Assignment 3
+// const language = 'English';
+// let population = '329.5 million';
+// const country = 'United States';
+// const continent = 'North America';
+// const isIsland = false;
+
