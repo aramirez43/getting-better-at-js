2d3ab42855f023d3e6fbfbfcab042112313eab5c
Alan Ramirez <aramireznicola@cnm.edu>
2021-12-16 23:49:30 -0700

    did some coding challeneges

--- js-fundamentals/script.js
+++ js-fundamentals/script.js
@@ -1,7 +1,7 @@
 //Assignment 1
 // let country = 'United States'
 // let continent = 'North America'
-// let population = '329.5 million'
+// let population = 329500000;
 //
 // console.log(country)
 // console.log(population)
@@ -64,3 +64,138 @@
 // const continent = 'North America';
 // const isIsland = false;
 
+//Assignment Operators
+//let x = 10 + 5;
+//x += 10; // x = x + 10 = 25
+//x *= 4; // x = x * 4 = 100
+//x++; // x = x + 1
+//x--; // x = x - 1
+
+//Comparison Operators
+//console.log(ageAlan > ageAlejandra) // >,<,>=,<=
+
+//Assignment 4
+// let halfOfPopulation = population / 2;
+// console.log(halfOfPopulation)
+// halfOfPopulation++
+// console.log(halfOfPopulation)
+// const finland = 6000000;
+// console.log(halfOfPopulation > finland)
+// let averageCountry = 33000000;
+// console.log(halfOfPopulation < averageCountry)
+// const description = country + ' is in ' + continent + ' and its ' + population + ' people speak ' + language;
+// console.log(description)
+
+//Coding Challenge 1
+// const marksWeight = 78;
+// const johnsWeight = 92;
+// const marksHeight = 1.69;
+// const johnsHeight = 1.95;
+// let marksBMI = marksWeight / marksHeight**2;
+// let johnsBMI = johnsWeight / johnsHeight**2;
+// console.log(johnsBMI, marksBMI)
+// let markHigherBMI = marksBMI > johnsBMI;
+// console.log(markHigherBMI)
+
+// strings & template literals
+// const firstName = 'Alan';
+// const job ='Web Developer';
+// const birthYear = 2000;
+// const year = 2037;
+//
+// const alan = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+// console.log(alan);
+
+//Template Literal
+// const alanNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+// console.log(alanNew)
+//
+// console.log(`just a regular string.....`);
+
+// this \n = new line
+// console.log('String with \n\
+// multiple \n\
+// lines')
+//
+// console.log(`String
+// Multiple
+// lines`)
+
+//Assignment 5
+// const description = `${country} is in ${continent} and its ${population} people speak ${language}`;
+// console.log(description)
+
+// const age = 15;
+
+//if isOldEnough is true the it will activate in curly braces
+// if (age >= 18) {
+//     console.log('Sarah can start driving license 🚗');
+// }else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years 😉`);
+// }
+//
+// const birthYear = 1991;
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// }else {
+//     century = 21;
+// }
+// console.log(century);
+
+//Assignment 6
+// let country = 'United States'
+// let continent = 'North America'
+// let population = 329500000;
+// const language = 'English';
+// let averageCountry = 33000000;
+//
+// if (population > averageCountry) {
+//     console.log(`${country} population is above average`)
+// }else {
+//     belowAverage = averageCountry - population;
+//     console.log(`${country} population is ${belowAverage} below average`)
+// }
+
+//Coding Challenge 2
+// const marksWeight = 78;
+// const johnsWeight = 92;
+// const marksHeight = 1.69;
+// const johnsHeight = 1.95;
+// const marksWeight = 95;
+// const johnsWeight = 85;
+// const marksHeight = 1.88;
+// const johnsHeight = 1.76;
+// let marksBMI = marksWeight / marksHeight**2;
+// let johnsBMI = johnsWeight / johnsHeight**2;
+// console.log(johnsBMI, marksBMI)
+// let markHigherBMI = marksBMI > johnsBMI;
+// console.log(markHigherBMI)
+//
+// if (markHigherBMI) {
+//     console.log(`Marks BMI(${marksBMI}) is higher than Johns(${johnsBMI})!`)
+// }else {
+//     console.log(`Johns BMI(${johnsBMI}) is higher than Marks(${marksBMI})!`)
+// }
+
+//type conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear))
+// console.log(Number(inputYear) + 18)
+// console.log(String(23))
+
+//type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3)
+
+// let n = '1' + 1; //'11'
+// n = n - 1;// 11-1 = 10
+// console.log(n);
+
+//Assignment 7
+'9'-'5';//4
+'19'-'13'+'17';//617
+'19'-'13'+17;//23
+'123'<57;// false
+5+6+'4'+9-4-2;//1143
