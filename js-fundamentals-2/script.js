b25359974274b1073756d29457d9339decfd2d63
Alan Ramirez <aramireznicola@cnm.edu>
2022-01-04 22:08:49 -0700

    Finished assignment 6

--- js-fundamentals-2/script.js
+++ js-fundamentals-2/script.js
@@ -161,27 +161,141 @@
 
 //Coding Challenge 1
 
-const calcAverage = (score1, score2, score3) => {
-    const totalAverage = (score1 + score2 + score3) / 3;
-    return totalAverage;
-}
-const dolphinsAvgScore = calcAverage(44, 23, 71);
-// console.log(dolphinsAvgScore);
-const koalasAvgScore = calcAverage(65, 54, 49);
-// console.log(koalasAvgScore)
+// const calcAverage = (score1, score2, score3) => {
+//     const totalAverage = (score1 + score2 + score3) / 3;
+//     return totalAverage;
+// }
+// const dolphinsAvgScore = calcAverage(44, 23, 71);
+// // console.log(dolphinsAvgScore);
+// const koalasAvgScore = calcAverage(65, 54, 49);
+// // console.log(koalasAvgScore)
 
-function checkWinner (avgDolphin, avgKoala) {
+// function checkWinner (avgDolphin, avgKoala) {
 
-    if  (avgKoala >= 2 * avgDolphin){
-        console.log(`Koalas win (${avgKoala} vs $ avgDolphin})`)
-    } else if (avgDolphin >= 2 * avgKoala) {
-        console.log(`Dolphins win ($ avgDolphin} vs ${avgKoala})`)
-    }else {
-        console.log('No teams win')
-    }
-}
+//     if  (avgKoala >= 2 * avgDolphin){
+//         console.log(`Koalas win (${avgKoala} vs $ avgDolphin})`)
+//     } else if (avgDolphin >= 2 * avgKoala) {
+//         console.log(`Dolphins win ($ avgDolphin} vs ${avgKoala})`)
+//     }else {
+//         console.log('No teams win')
+//     }
+// }
 
-checkWinner (dolphinsAvgScore, koalasAvgScore)
+// checkWinner (dolphinsAvgScore, koalasAvgScore)
+
+//Intro to arrays
+
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+// const y = new Array(1991, 1984, 2008, 2020);
+
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+
+// //length is a property and lets you know how many elements are in an array
+// console.log(friends.length);
+
+// //length is not zero based so it will get the friend in position two Peter
+// console.log(friends[friends.length - 1]);
+
+// //This changes Peter to Jay
+// //Arrays can always be changed even if constructor
+// //You cannot mutate the entire array
+// friends[2] = 'Jay';
+// console.log(friends);
+
+// //You can put many thing in an array
+// const firstName = 'Alan';
+// const alan = [firstName, 'Ramirez', 2037 - 2000, 'Web Developer', friends];
+// console.log(alan);
+
+// //exercise
+
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// //cant use operators on arrays
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+
+// console.log(ages);
+
+// const populations = [329.5, 129, 126, 51];
+
+// console.log(populations.length === 4);
+
+// function percentageOfWorld1(populationNum) {
+//     return (populationNum / 7900) * 100;
+// }
+
+// const percentages = [percentageOfWorld1(populations[0]), percentageOfWorld1(populations[1]), percentageOfWorld1(populations[2]), percentageOfWorld1(populations[3])];
+
+// console.log(percentages)
+
+//Array Operations Methods
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// //push is a method and is attached adds to array at the end
+// const newLength = friends.push('Jay');
+// console.log(friends);
+// console.log(newLength);
+
+// //unshift adds to the beginning
+// friends.unshift('John');
+// console.log(friends);
+
+// //pop removes elements last doesnt return number of elements in array
+// const popped = friends.pop();
+// friends.pop();
+// console.log(friends);
+// console.log(popped)
+
+// //shift removes from the beginning
+// friends.shift();
+// console.log(friends);
+
+// //indexOf lets you know position of element
+// console.log(friends.indexOf('Steven'));
+
+// //includes returns true if element is in array
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
+
+// if (friends.includes('Steven')) {
+//     console.log('You have a friend named Steven');
+// }
+
+//Assignment 6
+
+// const neighbors = ['Mexico', 'Canada'];
+// console.log(neighbors)
+// const newNeighbor = neighbors.push('Utopia');
+// console.log(neighbors)
+// const removedNeighbor = neighbors.pop();
+// console.log(neighbors)
+
+// if (!neighbors.includes('Germany')) {
+//     console.log('Probably not a central European country');
+// }
+
+// console.log(neighbors.indexOf('Mexico'));
+// neighbors[0] = 'Japan';
+// console.log(neighbors)
+
 
 
 
