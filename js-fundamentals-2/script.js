a316c24b6deccf8d8f81febf17892242ed4b5a35
Alan Ramirez <aramireznicola@cnm.edu>
2022-01-31 22:36:34 -0700

    started section 7

--- js-fundamentals-2/script.js
+++ js-fundamentals-2/script.js
@@ -758,45 +758,45 @@
 
 //Coding Challenge 4
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
-const tips = [];
+// const tips = [];
 
-const totals = [];
+// const totals = [];
 
-for (let i = 0; i < bills.length; i++) {
-    const tip = calcTip(bills[i])
-    tips.push(tip)
-    const total = tip + bills[i]
-    totals.push(total)
+// for (let i = 0; i < bills.length; i++) {
+//     const tip = calcTip(bills[i])
+//     tips.push(tip)
+//     const total = tip + bills[i]
+//     totals.push(total)
     
-}
+// }
 
-function calcTip(bill) {
+// function calcTip(bill) {
 
-    if (bill >= 50 && bill <= 300) {
-        const tip = bill * .15;
-        return tip
-    }else {
-        const tip = bill * .20;
-        return tip
-    }
-};
+//     if (bill >= 50 && bill <= 300) {
+//         const tip = bill * .15;
+//         return tip
+//     }else {
+//         const tip = bill * .20;
+//         return tip
+//     }
+// };
 
-function calcAverage(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        // sum = sum + arr[i]
-        sum += arr[i]
-    }
-    return sum / arr.length;
-}
+// function calcAverage(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         // sum = sum + arr[i]
+//         sum += arr[i]
+//     }
+//     return sum / arr.length;
+// }
 
-console.log(calcAverage([2, 3, 7]));
-console.log(calcAverage(totals));
+// console.log(calcAverage([2, 3, 7]));
+// console.log(calcAverage(totals));
 
-console.log(tips);
-console.log(totals);
+// console.log(tips);
+// console.log(totals);
 
 
 
---
+++ section 7 DOM/script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+//query selector selects the element class message then read its content
+// console.log(document.querySelector(".message").textContent);
+
+//DOM = Document Object Model
+//DOM is not part of JS
+
+//Changes the text to correct number
+// document.querySelector(".message").textContent = "Correct Number!";
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// //get value from input field
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+
+//Will use event listener for the check button to get value from input field
+//first argument listens for a click the second is the handler function to get value
+//input from UI is usually a string so we will convert the string into a number
+//math.random goes from 0-1 so * 20 brings it to 20
+//math.trunc makes everything a whole a number but wont go past 19.999
+//We will add a 1 to make it go to twenty
+//Store the score counter into the code instead of just the DOM
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  //if there is no number then return no number
+  if (!guess) {
+    //No input
+    document.querySelector(".message").textContent = "No Number!";
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
+
+    //Guess os too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost!";
+      document.querySelector(".score").textContent = 0;
+
+      //Guess is too low
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = " Start guessing ... ";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+});
